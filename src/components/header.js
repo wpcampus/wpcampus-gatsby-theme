67cd347e55d49a2cf859bab0ae3a9847e3c53bb4
Rachel Cherry <bamadesigner@gmail.com>
2019-10-29 16:23:14 -0400

    Replace header background color with WPCampus dark blue

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,7 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header>
     <div
       style={{
         margin: `0 auto`,
