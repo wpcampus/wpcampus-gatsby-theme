98dce5020181dde5e54955ba8312c455a1aaadaf
Rachel Cherry <bamadesigner@gmail.com>
2019-10-29 16:23:29 -0400

    Replace Gatsby astronaut with truck sheep

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "truck-sheep.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
