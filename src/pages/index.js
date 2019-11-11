b6b6c7e2348840d1596ff6ea9c683f2d972321bb
Rachel Cherry <bamadesigner@gmail.com>
2019-11-11 18:10:59 -0500

    Create Primary Nav and Nav component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import { NavPrimary } from "../components/nav"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
@@ -9,10 +9,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <p>WPCampus is a community of web professionals, educators, and people dedicated to advancing higher education by providing support, resources, and training focused on open source web publishing technologies.</p>
+    <NavPrimary/>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
