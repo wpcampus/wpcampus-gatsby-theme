9c3d59dc87cbd1d1a6bff1ca75eea6d4d6ce30e2
Rachel Cherry <bamadesigner@gmail.com>
2019-11-23 12:28:12 -0500

    Allow <Archive> component to pass <Article> settings

--- src/components/archive.js
+++ src/components/archive.js
@@ -3,19 +3,29 @@ import PropTypes from "prop-types"
 
 import Article from "../components/article"
 
-const Archive = ({ list }) =>
+const Archive = ({ list, displayMeta, displayContent, displayContentFull }) =>
   list.map(({ node }) => (
     <Article
       key={node.id}
       data={node}
       isSingle={false}
-      displayContent={true}
-      displayContentFull={false}
+      displayMeta={displayMeta}
+      displayContent={displayContent}
+      displayContentFull={displayContentFull}
     />
   ))
 
 Archive.propTypes = {
   list: PropTypes.array.isRequired,
+  displayMeta: PropTypes.bool,
+  displayContent: PropTypes.bool,
+  displayContentFull: PropTypes.bool,
+}
+
+Archive.defaultProps = {
+  displayMeta: true,
+  displayContent: true,
+  displayContentFull: false,
 }
 
 export default Archive
