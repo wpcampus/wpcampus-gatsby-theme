c0c68f896dfa95ae47427945ff6897a734e40b71
Rachel Cherry <bamadesigner@gmail.com>
2019-12-02 10:21:54 -0500

    Cleaning up the layout and main/header CSS a little

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./../css/layout.css"
+import "./../css/main.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,21 +27,11 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()},
-          {` `}
-          <a href="https://www.wpcampus.org">WPCampus</a>
-        </footer>
-      </div>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()},{` `}
+        <a href="https://www.wpcampus.org">WPCampus</a>
+      </footer>
     </>
   )
 }
