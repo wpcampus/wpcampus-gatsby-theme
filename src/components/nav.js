0800d1fee8781a89532fe5fb27dc1ab8393625c6
Rachel Cherry <bamadesigner@gmail.com>
2020-01-20 11:29:54 -0500

    Add Catego ries to primary nav as child of Blog

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,13 +2,32 @@ import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 
+const NavItem = ({ item }) => {
+  return (
+    <li>
+      <Link to={item.slug}>{item.text}</Link>
+      {item.children && item.children.length ? (
+        <ul>
+          {item.children.map((child, i) => (
+            <NavItem key={i} item={child} />
+          ))}
+        </ul>
+      ) : (
+        ""
+      )}
+    </li>
+  )
+}
+
+NavItem.propTypes = {
+  item: PropTypes.object.isRequired,
+}
+
 const Nav = ({ list }) => (
   <nav>
     <ul>
       {list.map((item, i) => (
-        <li key={i}>
-          <Link to={item.slug}>{item.text}</Link>
-        </li>
+        <NavItem key={i} item={item} />
       ))}
     </ul>
   </nav>
@@ -20,8 +39,13 @@ Nav.propTypes = {
 
 const NavPrimaryItems = [
   { slug: "/", text: "Home" },
-  { slug: "/blog/", text: "Blog" },
+  {
+    slug: "/blog/",
+    text: "Blog",
+    children: [{ slug: "/categories", text: "Categories" }],
+  },
   { slug: "/pages/", text: "Pages" },
+  { slug: "/members/", text: "Members" },
 ]
 
 const NavPrimary = () => <Nav list={NavPrimaryItems} />
