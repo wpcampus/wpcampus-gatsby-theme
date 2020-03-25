444e0f5de697506b407032e9dd6a3655d54f7c36
Rachel Cherry <bamadesigner@gmail.com>
2020-03-24 20:36:28 -0400

    Replacing <AuthorArchive> with <AuthorCards>

--- src/components/archive.js
+++ src/components/archive.js
@@ -3,29 +3,6 @@ import PropTypes from "prop-types"
 
 import Article from "../components/article"
 
-const AuthorArchive = ({ list }) => {
-	return list.map(({ node }) => {
-		// Convert author data to match post data for component
-		node.title = node.name
-		node.path = "/contributor/" + node.slug
-
-		return (
-			<Article
-				key={node.id}
-				data={node}
-				isSingle={false}
-				displayMeta={false}
-				displayContent={true}
-				displayContentFull={false}
-			/>
-		)
-	})
-}
-
-AuthorArchive.propTypes = {
-	list: PropTypes.array.isRequired,
-}
-
 const CategoryArchive = ({ list }) => {
 	return list.map(({ node }) => {
 		// Convert category data to match post data for component
@@ -83,4 +60,4 @@ ArticleArchive.defaultProps = {
 	displayContentFull: false,
 }
 
-export { ArticleArchive, AuthorArchive, CategoryArchive }
+export { ArticleArchive, CategoryArchive }
