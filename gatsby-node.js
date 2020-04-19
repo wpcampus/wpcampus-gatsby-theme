9984e8ae706f9b83090350ce559c30e3178ea61c
Rachel Cherry <bamadesigner@gmail.com>
2020-04-19 10:40:03 -0400

    Adding search back into the mix

--- gatsby-node.js
+++ gatsby-node.js
@@ -482,7 +482,7 @@ exports.createPages = async ({ graphql, actions }) => {
 	 */
 	createPage({
 		path: "/search/",
-		//matchPath: "/search/*",
+		matchPath: "/search/*",
 		component: path.resolve("src/templates/search.js")
 	})
 
