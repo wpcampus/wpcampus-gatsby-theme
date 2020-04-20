f4546ac8ce4f89b247b74d64985720105507e34d
Rachel Cherry <bamadesigner@gmail.com>
2020-04-20 10:33:10 -0400

    Adding "sidebar bottom" layout for Library template

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,6 +25,8 @@ const Layout = props => {
 		searchQuery,
 		updateSearchQuery,
 		isHome,
+		includeSidebar,
+		sidebarBottom,
 		useTitleTemplate,
 		heading,
 		crumbs,
@@ -48,6 +50,14 @@ const Layout = props => {
 		wpcampusAttr.className += " wpcampus--hasGridDev"
 	}
 
+	if (includeSidebar) {
+		wpcampusAttr.className += " wpcampus--sidebar"
+
+		if (sidebarBottom) {
+			wpcampusAttr.className += " wpcampus--sidebar--bottom"
+		}
+	}
+
 	if (isHome) {
 		wpcampusAttr.className += " wpcampus--home"
 	}
@@ -69,6 +79,25 @@ const Layout = props => {
 		crumbsComp = <Crumbs crumbs={crumbs} classes="wpc-area wpc-body__area wpc-body__area--crumbs" />
 	}
 
+	let sidebarArea
+	if (includeSidebar) {
+		sidebarArea = <div className="wpc-area wpc-body__area wpc-body__area--sidebar">
+			<Sidebar />
+		</div>
+	}
+
+	const bodyAreasAttr = {
+		className: "wpc-areas wpc-areas--grid wpc-areas--grid--rows wpc-body__areas"
+	}
+
+	if (includeSidebar) {
+		bodyAreasAttr.className += " wpc-body__areas--sidebar"
+
+		if (sidebarBottom) {
+			bodyAreasAttr.className += " wpc-body__areas--sidebar--bottom"
+		}
+	}
+
 	return (
 		<div {...wpcampusAttr}>
 			<a href="#main" className="wpc-skip-to-main">Skip to content</a>
@@ -78,7 +107,7 @@ const Layout = props => {
 			{!isHome ? <div className="wpc-hero"></div> : null}
 			<div className="wpc-body wpc-wrapper">
 				<div className="wpc-container wpc-body__container">
-					<div className="wpc-areas wpc-areas--grid wpc-areas--grid--rows wpc-body__areas">
+					<div {...bodyAreasAttr}>
 						<div className="wpc-area wpc-body__area wpc-body__area--nav">
 							<NavPrimary />
 						</div>
@@ -92,9 +121,7 @@ const Layout = props => {
 								{children}
 							</main>
 						</div>
-						<div className="wpc-area wpc-body__area wpc-body__area--sidebar">
-							<Sidebar />
-						</div>
+						{sidebarArea}
 					</div>
 				</div>
 			</div>
@@ -108,6 +135,8 @@ Layout.propTypes = {
 	searchQuery: PropTypes.string,
 	updateSearchQuery: PropTypes.func,
 	isHome: PropTypes.bool,
+	includeSidebar: PropTypes.bool,
+	sidebarBottom: PropTypes.bool,
 	pageTitle: PropTypes.string,
 	useTitleTemplate: PropTypes.bool,
 	heading: PropTypes.string,
@@ -119,6 +148,8 @@ Layout.propTypes = {
 
 Layout.defaultProps = {
 	isHome: false,
+	includeSidebar: true,
+	sidebarBottom: false,
 	useTitleTemplate: true
 }
 
--- src/templates/library.js
+++ src/templates/library.js
@@ -8,7 +8,7 @@ import Layout from "../components/layout"
 const Library = props => {
 	const page = props.data.wordpressPage
 	const library = props.data.allWordpressWpcLibrary.edges
-	return <Layout heading={page.title} crumbs={props.pageContext.crumbs} path={props.path}>
+	return <Layout heading={page.title} sidebarBottom={true} crumbs={props.pageContext.crumbs} path={props.path}>
 		<div>{ReactHtmlParser(page.content)}</div>
 		{library.map(({ node }, i) => {
 			return <div className="session" key={i}>
