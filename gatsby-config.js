d66576fd6ab4696b0be6d9596b88582de97efc19
Rachel Cherry <bamadesigner@gmail.com>
2020-05-13 14:07:08 -0400

    Remove Gatsby generator meta via plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
 		twitter: "@wpcampusorg",
 	},
 	plugins: [
+		"gatsby-plugin-remove-generator",
 		"gatsby-plugin-react-helmet",
 		{
 			resolve: "gatsby-plugin-react-helmet-canonical-urls",
