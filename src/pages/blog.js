ac8724ce03720bfe8e46d95ca73b4d42d07316d0
Rachel Cherry <bamadesigner@gmail.com>
2021-02-01 09:41:12 -0600

    Working on .wpc-callout styles, adding callout to main blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import { ArticleArchive } from "../components/archive"
+
+import "./../css/blog.css"
 
 export default function Template(props) {
 	const crumbs = {
@@ -17,6 +19,19 @@ export default function Template(props) {
 
 	return (
 		<Layout pageTitle="Community Blog" heading="The WPCampus Community Blog" crumbs={crumbs} path={props.path}>
+			<div className="blog-about">
+				<div>
+					<h2>About the blog</h2>
+					<p>The WPCampus Community Blog is the place for all primary and essential announcements related to our organization and community&#39;s growth.</p>
+					<p>If you&#39;re interested in helping plan our community, participate in surveys or looking for leadership and volunteer opportunities, visit the <Link to="/community/planning/">WPCampus Planning Blog</Link>.</p>
+				</div>
+				<div className="wpc-callout">
+					<h2>Subscribe to updates</h2>
+					<p>This mailing list sends an automated email that lets you know when we post to the WPCampus Community Blog.</p>
+					<a className="wpc-button wpc-button--dark" href="http://eepurl.com/dOd-Q9" target="_blank" rel="noreferrer">Subscribe to Community Blog updates</a>
+				</div>
+			</div>
+			<h2>Blog posts</h2>
 			<ArticleArchive list={props.data.allWordpressPost.edges} />
 		</Layout>
 	)
