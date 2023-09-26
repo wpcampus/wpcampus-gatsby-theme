a26677c0981ca91120c06a84656653e0b23d0635
Rachel Cherry <rachel@bamadesigner.com>
2023-09-26 14:11:56 -0400

    Clean up sidebar formatting and set blog limit to 3

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -25,13 +25,25 @@ const BlogWidget = () => {
 		require("@wpcampus/wpcampus-wc-blog")
 	}, [])
 	return useMemo(() => {
-		return <Widget type="blog">
-			<h2 className="wpc-widget__heading">
-				<span className="wpc-icon wpc-icon--quotes"></span>
-				<Link aria-label="The WPCampus Community Blog" to="/blog/">From our Community Blog</Link></h2>
-			<wpcampus-blog></wpcampus-blog>
-			<a className="wpc-button wpc-button--primary" href="http://eepurl.com/dOd-Q9" target="_blank" rel="noreferrer">Subscribe to Community Blog updates</a>
-		</Widget>
+		return (
+			<Widget type="blog">
+				<h2 className="wpc-widget__heading">
+					<span className="wpc-icon wpc-icon--quotes"></span>
+					<Link aria-label="The WPCampus Community Blog" to="/blog/">
+						From our Community Blog
+					</Link>
+				</h2>
+				<wpcampus-blog limit="3"></wpcampus-blog>
+				<a
+					className="wpc-button wpc-button--primary"
+					href="http://eepurl.com/dOd-Q9"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Subscribe to Community Blog updates
+				</a>
+			</Widget>
+		)
 	})
 }
 
